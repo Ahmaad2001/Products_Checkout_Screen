7a6b67215bc27d8363c24ea0eb92e624c0296e1a
Ahmaad2001 <ahmadmamounnaqawa@hotmail.com>
2024-03-13 13:09:01 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String? paymentMethod;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -69,24 +70,84 @@ class _MyAppState extends State<MyApp> {
                 children: [
                   Image.asset(
                     'assets/images/Frame.png',
-                    width: 30,
-                    height: 30,
+                    width: 41,
+                    height: 46,
                   ),
                   const SizedBox(
-                    width: 10,
+                    width: 16,
                   ),
                   const Text(
                     'Select Location',
                     style: TextStyle(fontSize: 16),
                   ),
                   const SizedBox(
-                    width: 170,
+                    width: 146,
                   ),
                   const Icon(
                     Icons.arrow_forward_ios,
                     size: 14,
                     color: Color.fromARGB(250, 153, 153, 153),
-                  )
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 500,
+              height: 78,
+              padding: const EdgeInsets.fromLTRB(15, 5, 10, 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 240, 232, 232),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/images/share_location.png',
+                    width: 46,
+                    height: 46,
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Saved Address',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        '389, Rue 233, Dubai, 0000',
+                        style: TextStyle(
+                          color: Color.fromARGB(250, 153, 153, 153),
+                        ),
+                      ),
+                      Text(
+                        'UAE',
+                        style: TextStyle(
+                          color: Color.fromARGB(250, 153, 153, 153),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 68,
+                  ),
+                  Radio(
+                    value: true,
+                    fillColor: const MaterialStatePropertyAll(Colors.orange),
+                    activeColor: Colors.orange,
+                    groupValue:
+                        true, // Assuming you always want it to be active
+                    onChanged: (bool? value) {
+                      // Handle onChanged if needed
+                    },
+                  ),
                 ],
               ),
             ),
@@ -103,34 +164,49 @@ class _MyAppState extends State<MyApp> {
             Container(
               width: 500,
               height: 78,
-              padding: const EdgeInsets.all(17),
+              padding: const EdgeInsets.fromLTRB(0, 10, 13, 10),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                      color: const Color.fromARGB(255, 240, 232, 232))),
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 240, 232, 232),
+                ),
+              ),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  const Text(
-                    'Online Payment',
-                    style: TextStyle(fontSize: 16),
+                  Expanded(
+                    child: RadioListTile(
+                      title: const Text("Online Payment"),
+                      value: "Online Payment",
+                      groupValue: paymentMethod,
+                      fillColor: const MaterialStatePropertyAll(Colors.orange),
+                      activeColor: Colors.orange,
+                      onChanged: (value) {
+                        setState(() {
+                          paymentMethod = value.toString();
+                        });
+                      },
+                    ),
                   ),
-                  const SizedBox(
-                    width: 85,
+                  Image.asset(
+                    'assets/images/Visa.png',
+                    width: 30,
+                    height: 30,
                   ),
-                  Image.asset('assets/images/Visa.png'),
-                  const SizedBox(
-                    width: 6,
+                  Image.asset(
+                    'assets/images/Mastercard.png',
+                    width: 30,
+                    height: 30,
                   ),
-                  Image.asset('assets/images/Mastercard.png'),
-                  const SizedBox(
-                    width: 6,
+                  Image.asset(
+                    'assets/images/DinersClub.png',
+                    width: 30,
+                    height: 30,
                   ),
-                  Image.asset('assets/images/DinersClub.png'),
-                  const SizedBox(
-                    width: 6,
+                  Image.asset(
+                    'assets/images/Discover.png',
+                    width: 30,
+                    height: 30,
                   ),
-                  Image.asset('assets/images/Discover.png'),
                 ],
               ),
             ),
@@ -138,19 +214,28 @@ class _MyAppState extends State<MyApp> {
             Container(
               width: 500,
               height: 78,
-              padding: const EdgeInsets.all(17),
+              padding: const EdgeInsets.fromLTRB(0, 10, 13, 10),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                      color: const Color.fromARGB(255, 240, 232, 232))),
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 240, 232, 232),
+                ),
+              ),
               child: Row(
                 children: [
-                  const Text(
-                    'Cash On Delivery',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(
-                    width: 175,
+                  Expanded(
+                    child: RadioListTile(
+                      title: const Text("Cash On Delivery"),
+                      value: "Cash On Delivery",
+                      groupValue: paymentMethod,
+                      fillColor: const MaterialStatePropertyAll(Colors.orange),
+                      activeColor: Colors.orange,
+                      onChanged: (value) {
+                        setState(() {
+                          paymentMethod = value.toString();
+                        });
+                      },
+                    ),
                   ),
                   Image.asset(
                     'assets/images/cash-on-delivery.png',
@@ -200,6 +285,56 @@ class _MyAppState extends State<MyApp> {
               ],
             ),
           ]),
+        ),
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1), // Shadow color
+                spreadRadius: 0.01, // Spread radius
+                blurRadius: 20, // Blur radius
+                offset: const Offset(0, -0.5), // Offset from top
+              ),
+            ],
+          ),
+          height: 138,
+          child: Column(
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text('Total Cost'), Text('300')],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              // ignore: sized_box_for_whitespace
+              Container(
+                width: 500,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.orange),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Place Order',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
